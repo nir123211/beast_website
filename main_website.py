6609c61fa4415211408792d40360feeb6203b5a1
nir <nir123211@gmail.com>
2022-11-18 19:03:01 +0200

    added template website

---
+++ main_website.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Welcome to the beast website')
+
+
---
+++ python scripts/utils.py
@@ -0,0 +1,48 @@
+import cv2 as cv
+import keyboard
+import time
+
+
+def resize_image(image_path, output_path, dimensions):
+    image = cv.imread(image_path)
+    image = cv.resize(image, dimensions)
+    cv.imwrite(output_path, image)
+
+
+def crop_image(image_path, output_path):
+    image = cv.imread(image_path)
+    min_width = 0
+    min_height = 0
+    max_width = image.shape[1]
+    max_height = image.shape[0]
+
+    image_ready = False
+    while image_ready is False:
+        cropped_image = image[min_height:max_height, min_width:max_width]
+        cv.imshow('image', cropped_image)
+        cv.waitKey(0)
+
+        if keyboard.is_pressed('up'):
+            print(max_height)
+            if max_height > 10:
+                max_height -= 10
+
+        if keyboard.is_pressed('down'):
+            if min_height < max_height-10:
+                min_height += 10
+
+        if keyboard.is_pressed('left'):
+            if max_width > 10:
+                max_width -= 10
+
+        if keyboard.is_pressed('right'):
+            if min_width < max_width-10:
+                min_width += 10
+
+        if keyboard.is_pressed('return'):
+            cv.imwrite(output_path, cropped_image)
+            image_ready = True
+
+
+# crop_image('../img/segev_and_me.jpg', '../img/segev_and_me2.jpg')
+resize_image('../img/segev_and_me2.jpg', '../img/carousel-1.jpg', (1920, 1080))
